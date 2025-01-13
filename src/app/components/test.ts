db6dcbf995d9532a9914da506aa5dd38661c7d47
Albertpm95 <Albertpm95@outlook.com>
2025-01-13 11:04:31 +0100

    Remove unused HTTP file upload logic from TestComponent

--- src/app/components/test.ts
+++ src/app/components/test.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { endpoints, environment } from 'environment';
+import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { DividerModule } from 'primeng/divider';
 import { FicherosServidorSeleccionadosComponent } from './lista-ficheros-servidor-seleccionados.component';
@@ -78,28 +75,4 @@ import { SubidaFicherosComponent } from './subida-fichero.component';
     DividerModule,
   ],
 })
-export class TestComponent {
-  http = inject(HttpClient);
-  fichero: any;
-  descripcionFichero = new FormControl('');
-  resultado: string = '';
-
-  seleccionarFicheroParaSubir(event: any) {
-    console.log('Subiendo fichero...', event);
-    this.fichero = event.target.files[0];
-  }
-
-  subirFichero() {
-    if (this.fichero) {
-      const formData = new FormData();
-      formData.append('file', this.fichero);
-
-      this.http
-        .post(`${environment.apiUrl + endpoints.utils.files.upload}`, formData)
-        .subscribe((response: any) => {
-          console.log('Respuesta:', response);
-          if (response.message) this.resultado = response.message;
-        });
-    }
-  }
-}
+export class TestComponent {}

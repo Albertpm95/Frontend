bef9b469b3fbbc1627ad5448a6b863cec6b316d2
Albertpm95 <Albertpm95@outlook.com>
2025-03-09 14:32:41 +0100

    Estilos

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -8,21 +8,13 @@ import { FileUploadViewerComponent } from '@app/components/file-upload-viewer..c
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <div>
-      <h1>Componentes para testar</h1>
-      <div class="d-flex flex-row">
-        <app-file-upload-viewer class="col-3"></app-file-upload-viewer
-        ><app-file-upload-viewer class="col-3"></app-file-upload-viewer
-        ><app-file-upload-viewer class="col-3"></app-file-upload-viewer
-        ><app-file-upload-viewer class="col-3"></app-file-upload-viewer>
-      </div>
+    <div class="flex space-x-4">
+      <app-file-upload-viewer class="flex-1"></app-file-upload-viewer>
+      <app-file-upload-viewer class="flex-1"></app-file-upload-viewer>
+      <app-file-upload-viewer class="flex-1"></app-file-upload-viewer>
     </div>
   `,
 })
 export class HomeComponent {
   constructor() {}
-
-  ngOnInit(): void {
-    // Initialization logic here
-  }
 }

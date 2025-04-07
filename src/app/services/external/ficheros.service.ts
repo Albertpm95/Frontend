47c08fa751d14ce8dab45d193e72c6346d028200
Albertpm95 <Albertpm95@outlook.com>
2025-04-07 08:30:55 +0200

    Cambios biome

--- src/app/services/external/ficheros.service.ts
+++ src/app/services/external/ficheros.service.ts
@@ -1,4 +1,4 @@
-import type { HttpClient } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable, inject } from "@angular/core";
 import { FilesListService } from "@services/internal/files-list.service";
 import { endpoints } from "endpoints";
@@ -10,8 +10,7 @@ import type { Observable } from "rxjs";
 })
 export class FicheroService {
 	#filesListService = inject(FilesListService);
-	constructor(private readonly http: HttpClient) {}
-
+	#http = inject(HttpClient);
 	ejecutarRegresionLinealMultiplesFicherosConcatenados(
 		test_size: number | undefined,
 		train_size: number | undefined,
@@ -20,8 +19,7 @@ export class FicheroService {
 		stratify: string | undefined,
 		intercept: boolean | undefined,
 		method: string | undefined,
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	): Observable<any> {
+	): Observable<object> {
 		const formData = new FormData();
 		if (test_size) formData.append("test_size", test_size.toString());
 		if (train_size) formData.append("train_size", train_size.toString());
@@ -34,20 +32,19 @@ export class FicheroService {
 		const files: { ficheros_matrices: File[]; ficheros_plot: File[] } =
 			this.#filesListService.getFiles();
 
-		files.ficheros_matrices.forEach((fichero: File) => {
+		for (const fichero of files.ficheros_matrices) {
 			if (fichero) {
 				formData.append("matrices", fichero);
 			}
-		});
+		}
 
-		files.ficheros_plot.forEach((fichero: File) => {
+		for (const fichero of files.ficheros_plot) {
 			if (fichero) {
 				formData.append("plots", fichero);
 			}
-		});
-		console.log("Enviando formdata ", formData);
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		return this.http.post<any>(
+		}
+
+		return this.#http.post<object>(
 			`${environment.apiUrl}${endpoints.linear_regresion.concatenacion_plot}`,
 			formData,
 		);
@@ -56,7 +53,7 @@ export class FicheroService {
 	enviarFicheroMat(file: File): Observable<string[]> {
 		const formData = new FormData();
 		formData.append("mat_file", file);
-		return this.http.post<string[]>(
+		return this.#http.post<string[]>(
 			`${environment.apiUrl}${endpoints.linear_regresion.analizar_fichero_mat}`,
 			formData,
 		);
